29618bc02916e017cbb3db05568d2dc0a2e5746c
LOMASH RISHI <lomashrishisinha@gmail.com>
2024-03-02 20:11:18 +0530

    TopSlider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: 'home', redirectTo: '', pathMatch: 'full' },
-    {'path':'','title':'HomePage',component:HomeComponent},
-    {'path':'about-us','title':'About-Us',component:AboutusComponent }
+    {'path': 'home', redirectTo: '', pathMatch: 'full' },
+    {'path':'','title':'Home-Page | Kanker Recruitment Portal',component:HomeComponent},
+    {'path': 'about', redirectTo: 'about-us', pathMatch: 'full' },
+    {'path':'about-us','title':'About-Us | Kanker Recruitment Portal',component:AboutusComponent },
+    {'path': 'contact', redirectTo: 'contact-us', pathMatch: 'full' },
+    {'path':'contact-us','title':'Contact-Us | Kanker Recruitment Portal',component:ContactusComponent},
+    {'path':'register','title':'User-Registration | Kanker Recruitment Portal',component:RegisterComponent},
+    {'path':'login','title':'User-Login | Kanker Recruitment Portal', component:LoginComponent}
+
+
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
