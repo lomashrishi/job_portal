db71876d6a50b6e25b71aa9d6bd354dfca520166
LOMASH RISHI <lomashrishisinha@gmail.com>
2024-03-05 15:11:25 +0530

    Slider Top Banner Updated

---
+++ src/app/info/info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  standalone: true,
+  imports: [InfoComponent],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+
+}
