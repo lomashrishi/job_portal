90a352c6a3ca898769783c06adaf2c07edf4ebbe
Lomash Rishi <lomashrishisinha@gmail.com>
2024-03-14 12:32:50 +0530

    Login Page Update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, FontAwesomeModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
