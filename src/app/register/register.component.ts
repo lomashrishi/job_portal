f3faefdf6d1793d97cecd25a37a555a277bdeada
Lomash Rishi <lomashrishisinha@gmail.com>
2024-03-29 13:50:39 +0530

    Register Componet Updated

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RegisterComponent,],
+  imports: [RegisterComponent,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+
+
+
+
+  // cancel(): void {
+  //   // Reset form fields
+  //   this.registrationForm.reset();
+  // }
 
 }
